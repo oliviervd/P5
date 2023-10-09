b0043f5dc754b38c73cb9932c7d3d6990cd2d2c3
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-10-09 21:39:11 +0200

    update sketch: rotationDevice

--- sketches /2023/rotationDevice.js
+++ sketches /2023/rotationDevice.js
@@ -2,15 +2,32 @@
 
 function setup() {
     createCanvas(windowWidth, windowHeight)
+    colorMode(RGB, 255, 255, 255, 1);
+
 }
 
 function draw() {
     background(0);
+    noStroke();
+
+    // SYSTEM 1 
+    push();
+
+    fill(255, 255, 255, 0.7);
+    ellipse(windowWidth/2, windowHeight/2, 300, 300);
+    pop();
+
+    // SYSTEM 2
+    rectMode(CENTER); 
     translate(windowWidth/2, windowHeight/2);
 
-    // rectangle
-    rectMode(CENTER); 
-    rotate(frameCount*0.01);
-    fill("#f1f1f1");
+    spinningLine(1); // first line
+    spinningLine(0.33) // second line
+}
+
+// GEOMATRIES 
+function spinningLine(speed) {
+    rotate(radians(frameCount)*speed)
+    fill(255, 255, 255, 0.8);
     rect(0, 0, 20, windowWidth*0.33);
 }
