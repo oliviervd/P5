e1adb66a7b7640c6cff69087bfd506e1129d4908
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-10-11 19:53:32 +0200

    add sketch: scrollingText

--- sketches /2023/typography/scrollingText.js
+++ sketches /2023/typography/scrollingText.js
@@ -1,9 +1,24 @@
-let text = "ALL THE SMALL THINGS"
+// inspired by https://timrodenbroeker.de/trusttheprocess4-data-stream/ 
+
+let txt = "The next big thing will be a lot of small things. The next big thing will be a lot of small things. ";
 
 function setup(){
-
+    createCanvas(windowWidth, windowHeight);
+    frameRate(5);
 }
 
 function draw(){
-    
+    background("#0000ff")
+    fill("#f1f1f1")
+    textAlign(LEFT, TOP)
+    textSize(20);
+    textLeading(160);
+    textFont("arial");
+
+    var last = txt[txt.length-1] // fetch last letter of the string
+    txt = last + txt  // append last letter of the text to the front.
+    txt = txt.substring(0, txt.length -1); // delete last letter of the text.  
+
+    text(txt,0, 0, windowWidth, windowHeight)
+    //console.log(txt)
 }
