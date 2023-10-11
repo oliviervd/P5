6c37a8e2bc78d11b5d7a692d7e4e52394a4b0ac7
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-10-11 20:21:16 +0200

    update sketch: scrolling text

--- sketches /2023/typography/scrollingText.js
+++ sketches /2023/typography/scrollingText.js
@@ -1,24 +1,29 @@
 // inspired by https://timrodenbroeker.de/trusttheprocess4-data-stream/ 
 
-let txt = "The next big thing will be a lot of small things. The next big thing will be a lot of small things. ";
+let txt = "The next big thing will be a lot of small things.";
 
 function setup(){
     createCanvas(windowWidth, windowHeight);
-    frameRate(5);
+    frameRate(10);
 }
 
 function draw(){
-    background("#0000ff")
-    fill("#f1f1f1")
-    textAlign(LEFT, TOP)
-    textSize(20);
-    textLeading(160);
+
+    background(0)
+    let c = color("#f1f1f1") 
+    c.setAlpha(200); // set alpha for text
+    fill(c) // set color for text 
+
+    translate(0, windowHeight*0.3); // center text in the middle of the screen
+
+    textAlign(LEFT, TOP) 
+    textSize(500);
+    //textLeading(160);
     textFont("arial");
 
     var last = txt[txt.length-1] // fetch last letter of the string
     txt = last + txt  // append last letter of the text to the front.
     txt = txt.substring(0, txt.length -1); // delete last letter of the text.  
 
-    text(txt,0, 0, windowWidth, windowHeight)
-    //console.log(txt)
+    text(txt.toUpperCase(),0, 0)
 }
